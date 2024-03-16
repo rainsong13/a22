07b3e5cecbb04b3775dbfc807981bcff3bcd0cbd
rainsong13 <159110910+rainsong13@users.noreply.github.com>
2024-03-16 01:25:08 -0400

    Add files via upload

---
+++ a2/goal.py
@@ -0,0 +1,170 @@
+"""CSC148 Assignment 2
+
+CSC148 Winter 2024
+Department of Computer Science,
+University of Toronto
+
+This code is provided solely for the personal and private use of
+students taking the CSC148 course at the University of Toronto.
+Copying for purposes other than this use is expressly prohibited.
+All forms of distribution of this code, whether as given or with
+any changes, are expressly prohibited.
+
+Authors: Diane Horton, David Liu, Mario Badr, Sophia Huynh, Misha Schwartz,
+Jaisie Sin, and Joonho Kim
+
+All of the files in this directory and all subdirectories are:
+Copyright (c) Diane Horton, David Liu, Mario Badr, Sophia Huynh,
+Misha Schwartz, Jaisie Sin, and Joonho Kim
+
+Module Description:
+
+This file contains the hierarchy of Goal classes and related helper functions.
+"""
+from __future__ import annotations
+import math
+import random
+from block import Block
+from settings import colour_name, COLOUR_LIST
+
+
+def generate_goals(num_goals: int) -> list[Goal]:
+    """Return a randomly generated list of goals with length <num_goals>.
+
+    Each goal must be randomly selected from the two types of Goals provided
+    and must have a different randomly generated colour from COLOUR_LIST.
+    No two goals can have the same colour.
+
+    Preconditions:
+    - num_goals <= len(COLOUR_LIST)
+    """
+    # TODO: Implement this function
+    return [PerimeterGoal(COLOUR_LIST[0])]  # FIXME
+
+
+def flatten(block: Block) -> list[list[tuple[int, int, int]]]:
+    """Return a two-dimensional list representing <block> as rows and columns of
+    unit cells.
+
+    Return a list of lists L, where,
+    for 0 <= i, j < 2^{max_depth - self.level}
+        - L[i] represents column i and
+        - L[i][j] represents the unit cell at column i and row j.
+
+    Each unit cell is represented by a tuple of 3 ints, which is the colour
+    of the block at the cell location[i][j].
+
+    L[0][0] represents the unit cell in the upper left corner of the Block.
+    """
+    # TODO: Implement this function
+
+
+class Goal:
+    """A player goal in the game of Blocky.
+
+    This is an abstract class. Only child classes should be instantiated.
+
+    Instance Attributes:
+    - colour: The target colour for this goal, that is the colour to which
+              this goal applies.
+    """
+    colour: tuple[int, int, int]
+
+    def __init__(self, target_colour: tuple[int, int, int]) -> None:
+        """Initialize this goal to have the given <target_colour>.
+        """
+        self.colour = target_colour
+
+    def score(self, board: Block) -> int:
+        """Return the current score for this goal on the given <board>.
+
+        The score is always greater than or equal to 0.
+        """
+        raise NotImplementedError
+
+    def description(self) -> str:
+        """Return a description of this goal.
+        """
+        raise NotImplementedError
+
+
+class PerimeterGoal(Goal):
+    """A goal to maximize the presence of this goal's target colour
+    on the board's perimeter.
+    """
+
+    def score(self, board: Block) -> int:
+        """Return the current score for this goal on the given board.
+
+        The score is always greater than or equal to 0.
+
+        The score for a PerimeterGoal is defined to be the number of unit cells
+        on the perimeter whose colour is this goal's target colour. Corner cells
+        count twice toward the score.
+        """
+        # TODO: Implement this method
+        return 148  # FIXME
+
+    def description(self) -> str:
+        """Return a description of this goal.
+        """
+        # TODO: Implement this method
+        return 'DESCRIPTION'  # FIXME
+
+
+class BlobGoal(Goal):
+    """A goal to create the largest connected blob of this goal's target
+    colour, anywhere within the Block.
+    """
+
+    def score(self, board: Block) -> int:
+        """Return the current score for this goal on the given board.
+
+        The score is always greater than or equal to 0.
+
+        The score for a BlobGoal is defined to be the total number of
+        unit cells in the largest connected blob within this Block.
+        """
+        # TODO: Implement this method
+        return 148  # FIXME
+
+    def _undiscovered_blob_size(self, pos: tuple[int, int],
+                                board: list[list[tuple[int, int, int]]],
+                                visited: list[list[int]]) -> int:
+        """Return the size of the largest connected blob in <board> that (a) is 
+        of this Goal's target <colour>, (b) includes the cell at <pos>, and (c)
+        involves only cells that are not in <visited>.
+
+        <board> is the flattened board on which to search for the blob.
+        <visited> is a parallel structure (to <board>) that, in each cell,
+        contains:
+            -1 if this cell has never been visited
+            0  if this cell has been visited and discovered
+               not to be of the target colour
+            1  if this cell has been visited and discovered
+               to be of the target colour
+
+        Update <visited> so that all cells that are visited are marked with
+        either 0 or 1.
+
+        If <pos> is out of bounds for <board>, return 0.
+        """
+        # TODO: Implement this method
+
+    def description(self) -> str:
+        """Return a description of this goal.
+        """
+        # TODO: Implement this method
+        return 'DESCRIPTION'  # FIXME
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'allowed-import-modules': [
+            'doctest', 'python_ta', 'random', 'typing', 'block', 'settings',
+            'math', '__future__'
+        ],
+        'max-attributes': 15
+    })
